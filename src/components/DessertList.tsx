7880af1baf658fa510a1623dcfc341860749c26b
Mr-wilz <juiciwhilf@gmail.com>
2025-06-08 12:33:12 +0100

    final commit

--- src/components/DessertList.tsx
+++ src/components/DessertList.tsx
@@ -7,7 +7,7 @@ const DessertList = () => {
 
   return (
     <div className="px-2 sm:px-4">
-      <div className="grid gap-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 sm:gap-1 rounded-xl p-2">
+      <div className="grid gap-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-3 sm:gap-1 rounded-xl p-2">
         {desserts.map((dessert, index) => (
           <DessertItem key={index} dessert={dessert} />
         ))}
